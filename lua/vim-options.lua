23618959c7c6c456161732676bc47f59c65b1c53
Michael Augaitis <michael.aug@proton.me>
2023-12-25 11:36:30 +0000

    Disable debug logs
    Update vim options.
    Add keybind for clearing search highlight

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,9 +1,16 @@
 -- General nvim options
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
--- To find debug log file, run :lua =require('vim.lsp.log').get_filename()
-vim.lsp.set_log_level("debug")
+vim.opt.nu = true -- Line numbers
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.scrolloff = 8  -- Show at least 8 lines and the end of file
+
+vim.keymap.set('n', '<leader>cs', ':noh<CR>', {}) -- Clear search highlight
+
+-- To find LSP debug log file, run :lua =require('vim.lsp.log').get_filename()
+-- vim.lsp.set_log_level("debug")
