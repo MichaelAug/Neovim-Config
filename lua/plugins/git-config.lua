cf70276f5db0441eee0b6dcf11e0d39c0888f2b2
Michael Augaitis <michael.aug@proton.me>
2023-12-28 23:47:30 +0000

    Add diffview and neogit

--- lua/plugins/git-config.lua
+++ lua/plugins/git-config.lua
@@ -73,4 +73,20 @@ return {
       end,
     },
   },
+
+  -- Git diff view
+  {'sindrets/diffview.nvim'},
+
+  -- Git interface
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true
+  }
 }
