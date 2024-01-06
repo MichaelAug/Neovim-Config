82e93a51c125acd5cb95622ad789fe1b6dadb678
Michael Augaitis <michael.aug@proton.me>
2024-01-06 22:04:35 +0000

    Use nvim-autopairs instead of mini.pairs
    autopairs does smarter pairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,13 +1,10 @@
 return {
   -- Automatically add matching brackets
   {
-    "echasnovski/mini.pairs",
-    version = "*",
-    config = function()
-      require("mini.pairs").setup()
-    end,
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    opts = {} -- this is equalent to setup({}) function
   },
-
   -- Surround text and movement keybinds 
   -- NOTE: these keybinds will show up as conflicting in whichkey checkhealth
   -- but that's fine, possibly a bug with which-key or mini-surround
