768f4db97b6b549201e4f1e42049739832edf8f2
Michael Augaitis <michael.aug@proton.me>
2024-07-19 13:00:51 +0100

    Change to LazyVim
    In Folke we trust

---
+++ lua/config/options.lua
@@ -0,0 +1,11 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.o.title = true
+
+-- Set these when using Neovide
+if vim.g.neovide then
+    vim.o.guifont = "Source Code Pro:h11"
+end
+
